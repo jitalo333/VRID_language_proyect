794a7c3e8d6f2acdede0c09317ae1b0ff062cbda
Alonso <alonso.valderrama@biomedica.udec.cl>
2025-08-26 14:21:49 -0400

    se agrega funcion gen_dataset

---
+++ BERT/utils/dataset.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def gen_dataset(codes_vrid, df):
+    #Selección unicamente de elementos de df que se encuentren en codes_vrid
+    df = df[df["Código VRID"].isin(codes_vrid)].copy()
+
+    #Creación de index en función de orden de los datos
+    df['idx'] = np.arange(0, df.shape[0])
+
+    #Generación de datasets
+    X = df["text_for_embedding_translated"]
+    y = df["Interdisciplinario"]
+    return X, y, df
